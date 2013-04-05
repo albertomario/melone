cc74c3882d9eae2e276af495c337868adad83464
Tim Helfensdörfer <tim@visualappeal.de>
2013-04-05 14:30:25 +0200

    Finished tests for the interface server.

--- tests/ServerTest.js
+++ tests/ServerTest.js
@@ -159,7 +159,7 @@ module.exports.server = {
 		test.expect(1);
 
 		httpTest.get('/templates/edit/1', function(err, res) {
-			test.notStrictEqual(err, null);
+			test.strictEqual(err, 404);
 
 			test.done();
 		});
@@ -188,6 +188,48 @@ module.exports.server = {
 
 					test.strictEqual(updatedTemplate.id, theTemplate.id);
 					test.strictEqual(updatedTemplate.name, 'Template update');
+
+					test.done();
+				});
+			});
+		});
+	},
+
+	testTemplateUpdateNotFound: function(test) {
+		test.expect(1);
+
+		httpTest.post('/templates/edit/1', {
+			name: 'Template update'
+		}, function(err, res) {
+			test.strictEqual(err, 404);
+
+			test.done();
+		});
+	},
+
+	testTemplateUpdateNotValidated: function(test) {
+		test.expect(5);
+
+		var theTemplate = Template.factory({
+			name: 'Template test',
+			description: '',
+			html: '<h1>Test</h1>',
+			plain: 'Test'
+		});
+
+		theTemplate.save(function(err) {
+			test.ifError(err);
+
+			httpTest.post('/templates/edit/' + theTemplate.id, {
+				name: ''
+			}, function(err, res) {
+				test.ifError(err);
+
+				Template.find(theTemplate.id, function(err, updatedTemplate) {
+					test.ifError(err);
+
+					test.strictEqual(updatedTemplate.id, theTemplate.id);
+					test.strictEqual(updatedTemplate.name, 'Template test');
 
 					test.done();
 				});
@@ -226,7 +268,7 @@ module.exports.server = {
 		test.expect(1);
 
 		httpTest.get('/templates/remove/1', function(err, res) {
-			test.notStrictEqual(err, null);
+			test.strictEqual(err, 404);
 
 			test.done();
 		});
@@ -257,6 +299,271 @@ module.exports.server = {
 
 				test.done();
 			});
+		});
+	},
+
+	testTagViewNotFound: function(test) {
+		test.expect(1);
+
+		httpTest.get('/tags/1', function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testTagAdd: function(test) {
+		test.expect(1);
+
+		httpTest.get('/tags/add', function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testTagCreate: function(test) {
+		test.expect(1);
+
+		httpTest.post('/tags/add', {
+			name: 'Tag 1'
+		}, function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testTagCreateError: function(test) {
+		test.expect(1);
+
+		httpTest.post('/tags/add', {
+			name: ''
+		}, function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testTagEdit: function(test) {
+		test.expect(2);
+
+		var theTag = Tag.factory({
+			name: 'Tag Test'
+		});
+
+		theTag.save(function(err) {
+			test.ifError(err);
+
+			httpTest.get('/tags/edit/' + theTag.id, function(err, res) {
+				test.ifError(err);
+
+				test.done();
+			});
+		});
+	},
+
+	testTagEditNotFound: function(test) {
+		test.expect(1);
+
+		httpTest.get('/tags/edit/1', function(err, res) {
+			test.strictEqual(err, 404);
+
+			test.done();
+		});
+	},
+
+	testTagUpdate: function(test) {
+		test.expect(5);
+
+		var theTag = Tag.factory({
+			name: 'Tag test',
+			description: ''
+		});
+
+		theTag.save(function(err) {
+			test.ifError(err);
+
+			httpTest.post('/tags/edit/' + theTag.id, {
+				name: 'Tag update'
+			}, function(err, res) {
+				test.ifError(err);
+
+				Tag.find(theTag.id, function(err, updatedTag) {
+					test.ifError(err);
+
+					test.strictEqual(updatedTag.id, theTag.id);
+					test.strictEqual(updatedTag.name, 'Tag update');
+
+					test.done();
+				});
+			});
+		});
+	},
+
+	testTagUpdateNotFound: function(test) {
+		test.expect(1);
+
+		httpTest.post('/tags/edit/1', {
+			name: 'Tag update'
+		}, function(err, res) {
+			test.strictEqual(err, 404);
+
+			test.done();
+		});
+	},
+
+	testTagUpdateNotValidated: function(test) {
+		test.expect(5);
+
+		var theTag = Tag.factory({
+			name: 'Tag test',
+			description: ''
+		});
+
+		theTag.save(function(err) {
+			test.ifError(err);
+
+			httpTest.post('/tags/edit/' + theTag.id, {
+				name: ''
+			}, function(err, res) {
+				test.ifError(err);
+
+				Tag.find(theTag.id, function(err, updatedTag) {
+					test.ifError(err);
+
+					test.strictEqual(updatedTag.id, theTag.id);
+					test.strictEqual(updatedTag.name, 'Tag test');
+
+					test.done();
+				});
+			});
+		});
+	},
+
+	testTagRemove: function(test) {
+		test.expect(4);
+
+		var theTag = Tag.factory({
+			name: 'Tag test',
+			description: ''
+		});
+
+		theTag.save(function(err) {
+			test.ifError(err);
+
+			httpTest.get('/tags/remove/' + theTag.id, function(err, res) {
+				test.ifError(err);
+
+				Tag.findAll(function(err, tags) {
+					test.ifError(err);
+
+					test.strictEqual(tags.length, 0);
+
+					test.done();
+				});
+			});
+		});
+	},
+
+	testTagRemoveNotFound: function(test) {
+		test.expect(1);
+
+		httpTest.get('/tags/remove/1', function(err, res) {
+			test.strictEqual(err, 404);
+
+			test.done();
+		});
+	},
+
+	testReportLinks: function(test) {
+		test.expect(1);
+
+		httpTest.get('/reports/links', function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testReportTags: function(test) {
+		test.expect(1);
+
+		httpTest.get('/reports/tags', function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testDocIndex: function(test) {
+		test.expect(1);
+
+		httpTest.get('/docs', function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testDocMail: function(test) {
+		test.expect(1);
+
+		httpTest.get('/docs/mail', function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testDocTemplate: function(test) {
+		test.expect(1);
+
+		httpTest.get('/docs/templates', function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testDocTag: function(test) {
+		test.expect(1);
+
+		httpTest.get('/docs/tags', function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testDataIndex: function(test) {
+		test.expect(1);
+
+		httpTest.get('/data/index', function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testDataTags: function(test) {
+		test.expect(1);
+
+		httpTest.get('/data/tags', function(err, res) {
+			test.ifError(err);
+
+			test.done();
+		});
+	},
+
+	testDataLinks: function(test) {
+		test.expect(1);
+
+		httpTest.get('/data/links', function(err, res) {
+			test.ifError(err);
+
+			test.done();
 		});
 	},
 
