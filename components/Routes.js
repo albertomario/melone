0df2da129b3493a844164c45dcc78676b979e085
Tim Helfensdörfer <tim@visualappeal.de>
2013-04-05 14:29:23 +0200

    Send error code with error template.

--- components/Routes.js
+++ components/Routes.js
@@ -26,13 +26,13 @@ NotFoundError.name = 'NotFoundError';
 
 var Routes = {
 	error: function(err, req, res, next) {
-		res.render('error.jade', {
+		res.status(err.code || 500).render('error.jade', {
 			error: err.message
 		}, function(renderError, html) {
 			if (renderError) {
 				res.send(500, 'Error while displaying the error template!');
 			} else {
-				res.send(err.code, html);
+				res.send(err.code || 500, html);
 			}
 		});
 	},
